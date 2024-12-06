e0b9276a59cd0b6491def0974120b07059556fa6
Nikita <vivat404@mail.ru>
2024-12-06 18:06:55 +0300

    reverse and prompt

--- main.go
+++ main.go
@@ -2,15 +2,42 @@ package main
 
 import "fmt"
 
+type account struct {
+	login    string
+	password string
+	url      string
+}
+
 func main() {
-	a := 5
+	login := promptData("Введите логин")
+	password := promptData("Введите пароль")
+	url := promptData("Введите url")
 
-	double(&a)
-	fmt.Println(a)
+	myAccount := account{
+		password: password,
+		login:    login,
+		url:      url,
+	}
 
+	outputPassword(&myAccount)
 }
 
-func double(num *int) {
-
-	*num = *num * 2
+func promptData(prompt string) string {
+	fmt.Print(prompt + ": ")
+	var res string
+	fmt.Scan(&res)
+	return res
 }
+
+func outputPassword(acc *account) {
+	fmt.Println(acc.login, acc.password, acc.url)
+}
+
+// a := [4]int{1, 2, 3, 4}
+// 	reverse(&a)
+// 	fmt.Print(a)
+// func reverse(arr *[4]int) {
+// 	for index, value := range *arr {
+// 		(*arr)[len(arr)-1-index] = value
+// 	}
+// }
