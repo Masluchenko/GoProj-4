d50670828a20e57d5df67478d72810502e1193b0
nikita <vivat404@mail.ru>
2024-12-16 00:38:15 +0300

    fix

---
+++ main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"fmt"
+	"math/rand/v2"
+	"net/url"
+)
+
+type account struct {
+	login    string
+	password string
+	url      string
+}
+
+func (acc account) outputPassword() {
+	fmt.Println(acc.login, acc.password, acc.url)
+}
+
+func newAccount(login, password, urlString string) (*account, error) {
+	_, err := url.ParseRequestURI(urlString)
+	if err != nil {
+		return nil, errors.New("Invalid URL") errorserr
+	}
+	return &account{
+		url:      urlString,
+		login:    login,
+		password: password,
+	}, nil
+}
+
+func (acc *account) generatePassword(n int) {
+	res := make([]rune, n)
+	for i := range res {
+		res[i] = letterRunes[rand.IntN(len(letterRunes))]
+	}
+	acc.password = string(res)
+}
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWYZ123456789*!")
+
+func main() {
+
+	login := promptData("Введите логин")
+	password := promptData("Введите пароль")
+	url := promptData("Введите url")
+
+	myAccount, err := newAccount(login, password, url)
+	if err != nil {
+		fmt.Println("Неверный формат URL")
+		return
+	}
+	myAccount.generatePassword(12)
+	myAccount.outputPassword()
+	fmt.Println(myAccount)
+}
+
+func promptData(prompt string) string {
+	fmt.Print(prompt + ": ")
+	var res string
+	fmt.Scan(&res)
+	return res
+}
+
+// func generatePassword(n int) string {
+// 	res := make([]rune, n)
+// 	for i := range res {
+// 		res[i] = letterRunes[rand.IntN(len(letterRunes))]
+// 	}
+// 	return string(res)
+// }
+
+// str := []rune("Привет!)")
+// for _, ch := range str {
+// 	fmt.Println(ch, string(ch))
+// }
+
+// a := [4]int{1, 2, 3, 4}
+// 	reverse(&a)
+// 	fmt.Print(a)
+// func reverse(arr *[4]int) {
+// 	for index, value := range *arr {
+// 		(*arr)[len(arr)-1-index] = value
+// 	}
+// }
